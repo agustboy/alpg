3008e11af3957589c049e8736570e619784e72ce
agustboy <80437089+agustboy@users.noreply.github.com>
2022-07-06 17:43:55 -0300

    Create conversaoGaloes.py

---
+++ conversaoGaloes.py
@@ -0,0 +1,9 @@
+import math
+g1, g2, g3 = [float(x) for x in input().split()]
+galao1 = g1 * 3,7854
+galao2 = g2 * 3,7854
+galao3 = g3 * 3,7854
+print(f"{g1} GALÕES -> {galao1} LITROS")
+print(f"{g2} GALÕES -> {galao2} LITROS")
+print(f"{g3} GALÕES -> {galao3} LITROS")
+
