4d94200d9230279e99d716cdeed93c66d972d29f
agustboy <80437089+agustboy@users.noreply.github.com>
2022-07-06 17:46:18 -0300

    Create insuranceCriteria.py

---
+++ insuranceCriteria.py
@@ -0,0 +1,14 @@
+gender = int(input())
+age = int(input())
+
+if gender == 1:
+  if age == 1:
+    print(1)
+  elif age == 0:
+    print(0)
+elif gender == 0:
+  if age == 1:
+    print(0)
+  elif age == 0:
+    print(0)
+  
