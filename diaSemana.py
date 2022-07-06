80726ad1c31da9b4b54a984a6558e7774a5f10e9
agustboy <80437089+agustboy@users.noreply.github.com>
2022-07-06 17:45:22 -0300

    Create diaSemana.py

---
+++ diaSemana.py
@@ -0,0 +1,18 @@
+dia = int(input())
+if dia < 1 or dia > 7:
+  print("valor invalido")
+else:
+  if dia == 1:
+    print("Domingo")
+  if dia == 2:
+    print("Segunda")
+  if dia == 3:
+    print("Terça")
+  if dia == 4:
+    print("Quarta")
+  if dia == 5:
+    print("Quinta")
+  if dia == 6:
+    print("Sexta")
+  if dia == 7:
+    print("Sabado")  
